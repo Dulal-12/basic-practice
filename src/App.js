941126487d38c646fbacfb89c79825589c44886a
Md.Dulal Miah <dulalmiah70112@gmail.com>
2021-02-01 20:55:14 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import {useState , useEffect} from 'react';
 function App() {
+  const [like , setLike] = useState('');
+  const [users , setUsers] = useState([]);
+  const [single , setSingle]  = useState({});
+  useEffect(()=>{
+                         fetch('https://jsonplaceholder.typicode.com/users')
+                         .then(res=>res.json())
+                         .then(data=>setUsers(data))
+
+                         fetch('https://jsonplaceholder.typicode.com/users/1')
+                         .then(res=>res.json())
+                         .then(data=> setSingle(data))
+
+  } , [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <ThumbUpAltIcon onClick = {()=>setLike(like ? '' : 'primary')} color = {like}></ThumbUpAltIcon>
+     <h4>Name : {single.name}</h4>
+     {
+       users.map(user => <li>{user.name}</li>)
+     }
     </div>
   );
 }
